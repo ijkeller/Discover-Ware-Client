c0fed274797eb664384d5e011de13c42a9815cea
Branden Ge <86636108+brandenge@users.noreply.github.com>
2022-09-05 00:52:43 -0500

    Fixed env variable name for API key to fix NoApiKey error in console

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
-const MapContainer = () => {
+function MapContainer() {
 
   const mapStyles = {
     height: "100vh",
@@ -13,12 +13,13 @@ const MapContainer = () => {
 
   return (
      <LoadScript
-      googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
+      googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
         <GoogleMap
           mapContainerStyle={mapStyles}
           zoom={13}
           center={defaultCenter}
-        />
+        >
+        </GoogleMap>
      </LoadScript>
   )
 }
