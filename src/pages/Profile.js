0556e89dec0fc736938cab850558117dd37941c1
Isaiah Keller <isaiah.keller@gmail.com>
2022-09-07 18:55:23 -0700

    cleaned profile, added rendering, added notes to updateLocation.

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,7 +1,6 @@
 import { Component } from "react";
 import { withAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
-// import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button'
 import Login from '../components/Login.js'
 import Logout from '../components/Logout.js'
@@ -22,6 +21,7 @@ class Profile extends Component {
         {
           name: 'initial state',
           address: 'initial state',
+          notes: 'initial state',
           image: 'initial state',
           types: ['initial state', 'initial state'],
           lat: 45,
@@ -80,16 +80,7 @@ class Profile extends Component {
       const updatedLocation = await axios(config);
       console.log('updatedLocation: ');
       console.table(updatedLocation);
-      this.getLocations(token)
-
-      // let updatedLocationsArray = this.state.favoriteLocations.map(origLocation => {
-      //   return origLocation._id === locationToUpdate._id
-      //     ? updatedLocation.data
-      //     : origLocation
-      // });
-      // this.setState({
-      //   favoriteLocations: updatedLocationsArray
-      // })
+      this.getLocations(token);
 
     } catch (error) {
       console.error('Error in handleUpdate: ', error);
@@ -127,18 +118,6 @@ class Profile extends Component {
                 <h4>{this.state.name}</h4>
                 <p>{this.state.email}</p>
                 <img src={this.state.picture} alt={` - ${this.state.name} - pic`} />
-                {/* <Form>
-                  <Form.Group >
-                    <Form.Label>Name:</Form.Label>
-                    <Form.Control type="text" placeholder="Enter Name" >
-
-                    </Form.Control>
-                  </Form.Group>
-                  <Form.Group >
-                    <Form.Label>Bio: </Form.Label>
-                    <Form.Control type="textarea" rows={4}></Form.Control>
-                  </Form.Group>
-                </Form> */}
               </div>
               <div className="favorite-locations" >
                 <h3>Favorite Locations</h3>
@@ -148,10 +127,10 @@ class Profile extends Component {
                       <div className="favorites-card" key={idx} >
                         <h3 className="location-name">{location.name}</h3>
                         <p className="address" >{location.address}</p>
-                        {/* <p className="notes" >{location.notes}</p> */}
-                        {/* <p className="location-type">{location.types.split(', ')}</p> */}
+                        <p className="notes" >{location.notes}</p>
+                        <p className="location-type">{location.types.split(', ')}</p>
                         <HomeIcon className="location-image" />
-                        {/* <img src={location.image} alt={location.name} /> */}
+                        <img className="location-image" src={location.image} alt={location.name} />
                         <UpdateLocation handleUpdate={this.handleUpdate} location={location} />
                         <Button onClick={() => this.handleDelete(location)} variant="secondary" >Delete Location</Button>
                       </div>
