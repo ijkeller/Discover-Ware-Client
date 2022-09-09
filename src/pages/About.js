d0cbcdd4be4f65357c4c6631b42951d41f9c6ea9
Isaiah Keller <isaiah.keller@gmail.com>
2022-09-09 09:55:49 -0700

    adjusted about page and css

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,30 +12,27 @@ class About extends Component {
 
   render() {
     return (
-      <>
-        <div className="about-container" >
-          {Data.map((person, idx) => {
-            return (
-                <Card className="card" key={idx} >
-                  <Card.Img variant="top" src={person.personImg} alt={`${person.personName} image`} />
-                  <Card.Body className="cardBody">
-                    <Card.Title className="title">{person.personName}</Card.Title>
-                    <Card.Text className="bio">{person.personData}</Card.Text>
-                    <Card.Link href={person.linkedin} className="linkedin">
-                      <Icon icon="logos:linkedin-icon" />
-                    </Card.Link>
-                    <Card.Link href={person.github} className="github">
-                      <Icon icon="icon-park:github" />
-                    </Card.Link>
-                  </Card.Body>
-                </Card>
-              )
-          })}
-        </div>
-        <div className="abouPage">
-
-        </div>
-      </>
+      <div className="about-container" >
+        {Data.map((person, idx) => {
+          return (
+            <Card className="card" key={idx} >
+              <Card.Title className="title" >{person.personName}</Card.Title>
+              <Card.Body className="cardBody">
+                <Card.Img className="card-image" variant="top" src={person.personImg} alt={`${person.personName} image`} />
+                <Card.Text className="bio">{person.personData}</Card.Text>
+                <div className="link-container" >
+                  <Card.Link href={person.linkedin} className="linkedin">
+                    <Icon style={{ fontSize: '1.5rem' }} icon="logos:linkedin-icon" />
+                  </Card.Link>
+                  <Card.Link href={person.github} className="github">
+                    <Icon style={{ fontSize: '1.5rem' }} icon="icon-park:github" />
+                  </Card.Link>
+                </div>
+              </Card.Body>
+            </Card>
+          )
+        })}
+      </div>
     );
   }
 }
