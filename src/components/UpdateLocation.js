faa0322fea1123fca6c7dea975d19b5477771e96
Isaiah Keller <isaiah.keller@gmail.com>
2022-09-09 07:26:28 -0700

    added delete button to notes and fixed modal css

--- src/components/UpdateLocation.js
+++ src/components/UpdateLocation.js
@@ -5,10 +5,10 @@ import Modal from 'react-bootstrap/Modal';
 
 export default class UpdateLocation extends React.Component {
     constructor(props) {
-      super(props)
-      this.state = {
-          showModal: false,
-      }
+        super(props)
+        this.state = {
+            showModal: false,
+        }
     }
 
     handleOpen = () => {
@@ -25,7 +25,24 @@ export default class UpdateLocation extends React.Component {
             _id: this.props.location._id,
             name: this.props.location.name,
             address: this.props.location.address,
-            notes: e.target.notes.value || this.props.location.notes ,
+            notes: e.target.notes.value || this.props.location.notes,
+            images: this.props.location.images,
+            types: this.props.location.types,
+            lat: this.props.location.lat,
+            lng: this.props.location.lng,
+            place_id: this.props.location.place_id,
+            __v: this.props.location.__v
+        }
+        this.props.handleUpdate(locationToUpdate)
+        this.handleClose()
+    }
+
+    handleDelete = () => {
+        let locationToUpdate = {
+            _id: this.props.location._id,
+            name: this.props.location.name,
+            address: this.props.location.address,
+            notes: '',
             images: this.props.location.images,
             types: this.props.location.types,
             lat: this.props.location.lat,
@@ -43,16 +60,19 @@ export default class UpdateLocation extends React.Component {
                 <Button size="sm" variant="secondary" onClick={this.handleOpen} > Update Notes</Button>
                 <Modal show={this.state.showModal} onHide={this.handleClose} centered className="modal" >
                     <Modal.Header closeButton></Modal.Header>
-                    <Modal.Title>Update Location</Modal.Title>
+                    <Modal.Title className='modal-title'>Update Location</Modal.Title>
                     <Modal.Body>
                         <Form onSubmit={this.handleSubmit}>
                             <Form.Group className="mb-3" controlId="notes">
                                 <Form.Control as="textarea" type="text" placeholder='Edit Notes' defaultValue={this.props.location.notes} />
                             </Form.Group>
-                            <Button className="button update-button" size="sm" variant="secondary" type="submit" >Add Note</Button>
                         </Form>
                     </Modal.Body>
                     <Modal.Footer>
+                        <div className='modal-footer-contents' >
+                            <Button className="button update-button" size="sm" variant="secondary" type="submit" >Add Note</Button>
+                            <Button className="button delete-button" size="sm" variant="danger" onClick={this.handleDelete} >Delete Note</Button>
+                        </div>
                     </Modal.Footer>
                 </Modal>
             </>
