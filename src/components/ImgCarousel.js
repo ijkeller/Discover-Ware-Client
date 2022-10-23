6efd1107e8c3b699a3ace60f160679b94f569533
Isaiah Keller <isaiah.keller@gmail.com>
2022-10-23 18:36:50 +0000

    unknown changes to src/components/ImgCarousel.js

--- src/components/ImgCarousel.js
+++ src/components/ImgCarousel.js
@@ -41,7 +41,7 @@ export default class ImgCarousel extends React.Component {
                         )
                     })}
                 </Carousel>
-                <Modal size="lg" show={this.state.showModal} onHide={this.handleClose} centered className='modal' >
+                <Modal size="xl" show={this.state.showModal} onHide={this.handleClose} centered className='modal' >
                     <Modal.Header closeButton></Modal.Header>
                     <Modal.Body>
                         <img className="modal-image" src={this.state.image} alt={`${this.props.location.name} img`} />
